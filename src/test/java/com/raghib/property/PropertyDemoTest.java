4db071bb510e8872fdd3151771db9730bc95dc28
usmaniraghib <javaprogrammers786@gmail.com>
2023-09-06 16:43:46 +0530

    Converted into maven project.

--- src/test/java/com/raghib/property/PropertyDemoTest.java
+++ src/test/java/com/raghib/property/PropertyDemoTest.java
@@ -1,27 +1,32 @@
 package com.raghib.property;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.raghib.config.Configuration;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 public class PropertyDemoTest {
 	Configuration config;
 	WebDriver webDriverObj = null;
+	ChromeOptions chromeOptions;
+	public static String browserArguments1 = "--disable-notifications";
+	public static String browserArguments2 = "--remote-allow-origins=*";
 	
 	@BeforeTest
 	public void setup() throws Exception {
 		config = new Configuration();
-		//System.setProperty(config.getChromeDriver(),config.getBrowserChromeDriver());
-		WebDriverManager.chromedriver().setup();
-		webDriverObj = new ChromeDriver();
-		webDriverObj.get(config.getApplicationURL());
+		chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments(browserArguments1,browserArguments2);
+		webDriverObj = new ChromeDriver(chromeOptions);
+		webDriverObj.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 		webDriverObj.manage().window().maximize();
+		webDriverObj.get(config.getApplicationURL());
 	}
 	
 	@Test
@@ -33,5 +38,4 @@ public class PropertyDemoTest {
 	public void setDown() {
 		webDriverObj.quit();
 	}
-
 }
